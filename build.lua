0823ba77c273b4a6c073e2ccdbe49bbd0b2fa9ea
cross-sniper <cts.contact.br@gmail.com>
2024-03-18 11:22:36 -0300

    added .gitignore

--- build.lua
+++ build.lua
@@ -1,6 +1,9 @@
 -- Build command
+if not arg or not table.concat(arg, " "):find("--skip-raylib") then
+    os.execute("cd raylib; cmake -S . -B build -DUSE_WAYLAND=ON; cd build; make")
+end
 
-local buildCommand = string.format("g++ src/main.cpp -o main -Os -O3 -I/usr/include/libpng16 -I/usr/include/harfbuzz -I/usr/include/freetype2 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/sysprof-6 -pthread -I/usr/include/AL -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system -L src/imgui -I src/imgui -limgui -lraylib -Wfatal-errors -Wall")
+local buildCommand = "g++ src/main.cpp -DDEBUG_BUILD -o main -Os -O3 -I/usr/include/libpng16 -I/usr/include/harfbuzz -I/usr/include/freetype2 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/sysprof-6 -pthread -I/usr/include/AL -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system -L src/imgui -I src/imgui -limgui -lraylib -DUSE_WAYLAND=ON -Wfatal-errors -Wall -L raylib/build/raylib -I raylib/build/raylib -lraylib"
 -- Execute build command
 print(buildCommand)
 local success = os.execute(buildCommand)
--- src/window.cpp
+++ src/window.cpp
@@ -1,19 +1,20 @@
-//window.cpp
+// window.cpp
 #include "defs.hpp"
-void drawWindows(){
-	rlImGuiBegin();
-    ImGui::Begin("info");
-	#ifdef DEBUG_BUILD
-    // Display player information
-    ImGui::Text("Player Position: (%.1f, %.1f)", player.pos.x, player.pos.y);
-    ImGui::Text("Player Health: %.1f", player.hp);
-    ImGui::Text("Player Iframe: %d", player.iframe);
-    #endif
-    ImGui::Text("You can quit by pressing \"ESC\"");
-    ImGui::Text("chrono-danger is still in development");
-    for(Upgrade up : Upgrades){
-        up.draw();
-    }
-    ImGui::End();
-	rlImGuiEnd();
+void drawWindows() {
+  rlImGuiBegin();
+  ImGui::Begin("info");
+#ifdef DEBUG_BUILD
+  // Display player information
+  ImGui::Text("Player Position: (%.1f, %.1f)", player.pos.x, player.pos.y);
+  ImGui::Text("Player Health: %.1f", player.hp);
+  ImGui::Text("Player Iframe: %d", player.iframe);
+  ImGui::Text("Player bullets: %d", player.bullets);
+#endif
+  ImGui::Text("You can quit by pressing \"ESC\"");
+  ImGui::Text("chrono-danger is still in development");
+  for (Upgrade up : Upgrades) {
+    up.draw();
+  }
+  ImGui::End();
+  rlImGuiEnd();
 }
